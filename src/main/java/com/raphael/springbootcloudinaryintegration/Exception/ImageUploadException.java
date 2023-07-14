3015f23ec8e19999f8677c074dda039188bc6db5
Raphael Ekpei <ekpeiraphael020@gmail.com>
2023-07-14 07:27:09 +0100

    Commit 11004

---
+++ src/main/java/com/raphael/springbootcloudinaryintegration/Exception/ImageUploadException.java
@@ -0,0 +1,7 @@
+package com.raphael.springbootcloudinaryintegration.Exception;
+
+public class ImageUploadException extends RuntimeException {
+    public ImageUploadException(String message) {
+        super(message);
+    }
+}
